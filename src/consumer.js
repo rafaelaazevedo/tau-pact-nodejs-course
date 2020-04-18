b79cb04705c6cad3933d8cd955ce2d214b9bc87f
Rafaela Azevedo <rafaelaazevedosouza@alphafx.co.uk>
2020-04-18 17:51:48 +0100

    fixed consumer and provider tests

--- src/consumer.js
+++ src/consumer.js
@@ -29,7 +29,7 @@ const getClient = async (id) => {
 
 const postClient = async (body) => {
       const res = await axios
-      .post(`${getApiEndpoint}/clients`, body, {'Content-Type': 'application/json'})
+      .post(`${getApiEndpoint}/clients`, body, {'Content-Type': 'application/json;charset=utf-8'})
       .then((res) => {
           return res
         })
